ebc0c5e82fee6c4455ecc4b58f09ec48a15a3497
ranaarvind3511 <124048554+ranaarvind3511@users.noreply.github.com>
2024-04-17 17:39:30 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -50,10 +50,10 @@ variable "network_security_groups" {
       direction                   = string
       access                      = string
       protocol                    = string
-      source_port_ranges          = list(string)
-      destination_port_ranges     = list(string)
+      source_port_range           = string
+      destination_port_range      = string
       source_address_prefix       = string
-      destination_address_prefixes= list(string)
+      destination_address_prefix  = string
       description                 = string
     }))
   }))
