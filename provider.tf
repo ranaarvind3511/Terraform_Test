a6beba245c08067091a4fafeec2cc901b37be60b
ranaarvind3511 <124048554+ranaarvind3511@users.noreply.github.com>
2024-04-17 17:44:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,18 +8,18 @@ terraform {
 
   # Configure the backend for Terraform Cloud
   backend "remote" {
-    organization = "vinod0510"
+    organization = "organization-name"
 
     workspaces {
-      name = "Terraform_Test"
+      name = "workspace-name"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  client_id       = "f72466e4-c2f5-408c-aca6-d8d7fa1f8043"
-  client_secret   = "bFx8Q~pKMRcRPaF_mIQAYUYisru1RxAmctssdcQE"
-  subscription_id = "1351724e-baf2-4e75-9ca5-4bc7b8d45471"
-  tenant_id       = "0e855965-35c9-4f2b-b5e5-cfae8dbb30d5"
+  client_id       = "XXX"
+  client_secret   = "XXX"
+  subscription_id = "XXX"
+  tenant_id       = "XXX"
 }
