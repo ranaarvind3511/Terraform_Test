bd9ad4cb4783c1579fda7120e7f13451427ad231
ranaarvind3511 <124048554+ranaarvind3511@users.noreply.github.com>
2024-04-18 11:58:41 +0530

    Update and rename provider.tf to Terraform_Cloud/modules /azure_vnet/provider.tf

--- Terraform_Cloud/modules /azure_vnet/provider.tf
+++ Terraform_Cloud/modules /azure_vnet/provider.tf
@@ -18,8 +18,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = "f72466e4-c2f5-408c-aca6-d8d7fa1f8043"
-  client_secret   = "bFx8Q~pKMRcRPaF_mIQAYUYisru1RxAmctssdcQE"
-  subscription_id = "1351724e-baf2-4e75-9ca5-4bc7b8d45471"
-  tenant_id       = "0e855965-35c9-4f2b-b5e5-cfae8dbb30d5"
+  client_id       = "xxx"
+  client_secret   = "xxx"
+  subscription_id = "xxx"
+  tenant_id       = "xxx"
 }
